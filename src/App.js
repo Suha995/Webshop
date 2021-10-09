c547ad77de78a1182c33794c0db0fd2fc593ebb1
Suhair Aljammal <saljammal995@gmail.com>
2021-10-09 17:11:36 +0200

    add registration

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
-
+import { useState } from 'react';
 import './App.scss';
 
 import Login from './components/Login';
 
 function App() {
+  const [ registered, setRegistered ] = useState(true);
   return (
     <div className="App">
-      <Login />
+      <Login registered={registered}/>
     </div>
   );
 }
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,26 +1,52 @@
-import { useState } from 'react';
-const Login = () => {
-    const [ email, setEmail ] = useState('');
-    const [ password, setPassword ] = useState('');
+import { useState } from "react";
+import './Login.scss';
 
-    const handleEmail = (e) => {
-        setEmail(e.target.value);
-    }
-    const handlePassword = (e) => {
-        setPassword(e.target.value);
-    }
-    const submitForm = (e) =>{
-        e.preventDefault();
-        console.log(email);
-        console.log(password);
-    }
+const Login = (props) => {
+  const [firstName, setFirstName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleFirstName = (e) => {
+    setFirstName(e.target.value);
+  };
+  const handleEmail = (e) => {
+    setEmail(e.target.value);
+  };
+  const handlePassword = (e) => {
+    setPassword(e.target.value);
+  };
+  const submitForm = (e) => {
+    e.preventDefault();
+    console.log(email);
+    console.log(password);
+  };
   return (
     <div className="form-signin">
       <form onSubmit={submitForm}>
-        <h2>Please sign in </h2>
-        <input type='text' placeholder='Email Address' name={email} onChange={handleEmail} />
-        <input type='password' placeholder='Password' name={password} onChange={handlePassword} />
-        <button type='submit'>Submit</button>
+        <h2>Please {props.registered ? "sign in" : "Register"} </h2>
+        {!props.registered && (
+          <input
+            type="text"
+            placeholder="Name"
+            name={firstName}
+            onChange={handleFirstName}
+          />
+        )}
+        <input
+          type="text"
+          placeholder="Email Address"
+          name={email}
+          onChange={handleEmail}
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          name={password}
+          onChange={handlePassword}
+        />
+        <button type="submit">
+          {props.registered ? "sign in" : "Register"}
+        </button>
         <p>&copy; 2021</p>
       </form>
     </div>
